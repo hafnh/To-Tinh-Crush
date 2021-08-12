b0b2f97ff8a7671ad2f154406d1f918b8db2c987
hafnh <88822732+hafnh@users.noreply.github.com>
2021-08-12 16:10:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Này đầu đất',
+    introTitle: 'Ê Quốc lởn',
     introDesc: `Em thấy nhớ anh, em
     Em thấy rất nhớ anh thoi
     Ngày đêm cứ loay hoay
@@ -7,15 +7,15 @@ const CONFIG = {
     Anh có nhớ em không?
     Anh có thấy nhớ em không?`,
     btnIntro: 'Hic hic 🥺🥺',
-    title: 'Anh mà thoát ra coi chừng khỏi cho chơi game đấy 😌😌',
-    desc: 'Anh có yêu em nhiều hongggg 🥺🥺',
+    title: 'M mà thoát ra là cái chày đâm vào mồm liền Quốc nầy🙂',
+    desc: 'Anh có yêu em nhiều khumm 🥺🥺',
     btnYes: 'Yêu nhiều lắm luôn, muốn ăn luôn 🥺🥺',
-    btnNo: 'Hong, anh yêu bé hàng xóm 😗😗',
-    question:'Vì sao anh lại yêu em dợoooo 🥺🥺',
-    btnReply: 'Gửi cho em nà 😝😝',
+    btnNo: 'Anh đéu yêu em 😞',
+    question:' Lí do anh yêu em ik❤️',
+    btnReply: 'Gửi cho em nè ❤️',
     reply: 'Vì em là em bé của mình anh thoiiii 😘😘',
     mess: 'Em biết mà, hihi 😚😚',
-    messDesc: 'Vậy thì bớt chơi game lại đi:))',
+    messDesc: ' Rứa thì bớt chơi game lại được ko 🙂',
     btnAccept: 'Dạ, anh biết gòi. Yêu emmmmmmm 🙆🙆',
-    messLink: 'http://fb.com' //messages/t/100014015913631
+    messLink: 'http://fb.com' //messages/t/100022524874764
 }
